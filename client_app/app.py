e421021a556c9cb7c218d1f5a40433d8491bd871
Nevin Baiju <nevinbaiju@gmail.com>
2024-06-19 15:22:53 -0400

    Added polling to redis only when client accesses webpage

--- client_app/app.py
+++ client_app/app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 
 import redis
 import json
 import time
 import os
+import threading
 
 redis_host = os.getenv('REDIS_HOST')
 redis_port = int(os.getenv('REDIS_PORT'))
 redis_password = os.getenv('REDIS_PASSWORD')
 
-print(redis_host, redis_port, redis_password)
-
 app = Flask(__name__)
 socketio = SocketIO(app)
+start_task = threading.Event()
+start_task.clear()
+connected_clients = 0
+lock = threading.Lock()
+
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password)
 
 @app.route('/')
@@ -21,8 +25,9 @@ def index():
     return render_template('index.html')
 
 def background_task():
-    """Background task that sends 'hi' every second."""
-    while True:
+    global connected_clients, start_task
+    while start_task.is_set() and connected_clients:
+        print('tasking ', connected_clients)
         time.sleep(0.1)
 
             # Connect to the Redis server
@@ -44,12 +49,20 @@ def background_task():
 
 @socketio.on('connect', namespace='/flights')
 def handle_connect():
-    print('Client connected')
-    socketio.start_background_task(background_task)
+    global start_task, connected_clients
+    with lock:
+        connected_clients += 1
+        if connected_clients == 1:
+            start_task.set()
+            socketio.start_background_task(background_task)
 
 @socketio.on('disconnect', namespace='/flights')
 def handle_disconnect():
-    print('Client disconnected')
+    global start_task, connected_clients
+    with lock:
+        connected_clients -= 1
+        if connected_clients == 0:
+            start_task.clear()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
